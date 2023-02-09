756f52a6d11796cc3a600986c6509291d4fac974
Lilytreasure <lilyngure@gmail.com>
2023-02-09 12:40:42 +0300

    splash updates

--- app/src/main/java/com/example/newsapp/SplashActivity.kt
+++ app/src/main/java/com/example/newsapp/SplashActivity.kt
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class SplashActivity : AppCompatActivity() {
+
+    //Entry to splash screen
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
